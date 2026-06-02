02ca191721448ad850dd6659bc4b793c13435c78
Favour Uweja <uwejafavour@gmail.com>
2026-06-02 19:43:35 +0100

    v

--- src/main/kotlin/com/xapps/question_generator/job/persistence/entity/NoteSummaryCreationJobDocument.kt
+++ src/main/kotlin/com/xapps/question_generator/job/persistence/entity/NoteSummaryCreationJobDocument.kt
@@ -7,7 +7,7 @@ import com.xapps.time.types.KotlinInstant
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document(collection = "note_summary_creation_jobs")
+@Document("note_summary_creation_jobs")
 data class NoteSummaryCreationJobDocument(
     @Id
     val jobId: String,
--- src/main/kotlin/com/xapps/question_generator/job/persistence/repository/NoteSummaryCreationJobMongoRepository.kt
+++ src/main/kotlin/com/xapps/question_generator/job/persistence/repository/NoteSummaryCreationJobMongoRepository.kt
@@ -1,7 +1,6 @@
 package com.xapps.question_generator.job.persistence.repository
 
 import com.xapps.question_generator.job.persistence.entity.NoteSummaryCreationJobDocument
-import com.xapps.question_generator.job.persistence.entity.NoteSummaryGenerationSpecDocument
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
